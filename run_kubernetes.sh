08ebcc1995235d26fec06f1625cb1d6df154b214
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2020-05-11 00:29:45 +0100

    Update run_kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="vahiwe/flaskapp"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskapp\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=flaskapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward flaskapp 8000:80
